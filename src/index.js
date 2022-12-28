bb04acbf273904e4e04921e2245a52c6e1f269bb
nataliapovaliy <natalia.bg1125@gmail.com>
2022-12-28 14:38:22 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,51 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchSearch } from './js/fetchSearch';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector(".gallery");
+const loadMoreBtn = document.querySelector(".load-more");
+
+let page = 1;
+let pages = 0;
+let q = "";
 
 searchForm.addEventListener("submit", onSearch);
+loadMoreBtn.addEventListener("click", onLoadMore);
 
 function onSearch(event) {
     event.preventDefault();
 
-    const q = event.currentTarget.searchQuery.value;
-    console.log(q);
+    q = event.currentTarget.searchQuery.value;
+
+    if (q === "") {
+        gallery.innerHTML = "";
+        return;
+    }
 
     fetchSearch(q)
         .then(data => {
             if (data.hits.length === 0) {
+                gallery.innerHTML = "";
                 Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
             }
+            if (data.hits.length > 0) {
+                Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+                
+                const forCard = data.hits;
+                markupCard(forCard);
+            }
+        })
 
-            const forCard = data.hits;
-            console.log(data);
-            console.log(forCard);
-            Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        .catch(error => {
+            Notiflix.Notify.failure('Error. Please try again.');
+        })
+}
 
-            for (const item of forCard) {
+function markupCard(forCard) {
+    for (const item of forCard) {
                 const htmlCars = `<div class="photo-card">
                                     <a href="${item.largeImageURL}">
                                         <img src="${item.webformatURL}" alt="" loading="lazy" width="250px" heigth="250px"/></a>
@@ -38,14 +57,18 @@ function onSearch(event) {
                                     </div>
                                 </div> `
                 gallery.innerHTML += htmlCars;
-            }
-        })
-        .catch(error => {
-            Notiflix.Notify.failure('Error. Please try again.');
-        })
-    
-    // let lightbox = new SimpleLightbox('.gallery a');
-    // gallery.on('show.simplelightbox');
-
+    }
+    let lightbox = new SimpleLightbox('.photo-card a', {});
 }
 
+async function onLoadMore() {
+    page += 1;
+    const response = await fetchSearch(q, page);
+    markupCard(response.hits);
+    pages += response.hits.length;
+
+    if (pages === response.totalHits) {
+        loadMoreBtn.className.add('is-hidden');
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+    }
+}
--- src/js/fetchSearch.js
+++ src/js/fetchSearch.js
@@ -1,16 +1,15 @@
-const key = '17988471-599edd7bd30320e243c69934a';
-const searchParams = new URLSearchParams({
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: true,
-});
+import axios from 'axios';
 
-export function fetchSearch(q) {
-    return fetch(`https://pixabay.com/api/?key=${key}&q=${q}&${searchParams}`)
-    .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-        return response.json();
-        })
+export async function fetchSearch(q, page) {
+    const key = '17988471-599edd7bd30320e243c69934a';
+    const searchParams = new URLSearchParams({
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        per_page: 20,
+});
+    
+    return await axios.get(`https://pixabay.com/api/?key=${key}&q=${q}&${searchParams}&page=${page}`)
+        .then(response => response.data);
+    
 }
